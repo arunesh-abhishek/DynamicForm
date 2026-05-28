c450c3f7d65048aebb996f12640fadf13eb135ae
Arunesh Abhishek <aruneshabhishek856@gmail.com>
2026-05-28 17:00:39 +0530

    Initial Commit

---
+++ DynamicForm/DynamicFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormApp.swift
+//  DynamicForm
+//
+//  Created by Arunesh Abhishek on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamicFormTests/DynamicFormTests.swift
@@ -0,0 +1,19 @@
+//
+//  DynamicFormTests.swift
+//  DynamicFormTests
+//
+//  Created by Arunesh Abhishek on 28/05/26.
+//
+
+import Testing
+@testable import DynamicForm
+
+struct DynamicFormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
